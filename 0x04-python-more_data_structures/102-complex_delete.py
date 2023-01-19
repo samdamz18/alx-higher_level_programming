fdecc137df614f0ff3446da924c9522dd9f4b738
Samuel Ojo <samdamz18@gmail.com>
2023-01-19 10:52:12 +0100

    Another commit

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
+
+    new = a_dictionary.copy()
+    for k, v in new.items():
+        if v == value:
+            del a_dictionary[k]
+
+    return a_dictionary
